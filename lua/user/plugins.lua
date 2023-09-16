4ba439c57b3418899408ab03f7f7e5aec88a17cb
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-16 23:52:58 +0530

    add gitsigns plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,6 +47,7 @@ return packer.startup(function(use)
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff
   use 'JoosepAlviste/nvim-ts-context-commentstring' -- type of comments to use
+  use "lewis6991/gitsigns.nvim"
 
   use "folke/tokyonight.nvim"   -- colorscehme
   use "LunarVim/darkplus.nvim"
