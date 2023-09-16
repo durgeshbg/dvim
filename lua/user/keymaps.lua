555b08c1643c13f28dc87f97a14e96ebef9daae8
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-16 20:42:42 +0530

    update keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,21 +10,14 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
---   term_mode = "t",
---   command_mode = "c",
-
 -- Normal --
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
-
-keymap("n", "o", "o<Esc>", opts)
-keymap("n", "O", "O<Esc>", opts)
-
-keymap("n", "<leader>w", ":w<CR>", opts)
-keymap("n", "<leader>q", ":wq<CR>", opts)
+keymap("n", "<leader>e", ":Lex 25<cr>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", ">", ":bn<CR>", opts)
+keymap("n", "<", ":bp<CR>", opts)
+
+keymap("n", "H", ":nohlsearch<CR>", opts)
 
 -- Insert --
 keymap("i", "jj", "<ESC>", opts)
@@ -34,13 +27,13 @@ keymap("i", "jj", "<ESC>", opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "J", ":m .+1<CR>==", opts)
+keymap("v", "K", ":m .-2<CR>==", opts)
+keymap("x", "<leader>j", "<ESC>", opts)
 keymap("v", "p", '"_dP', opts) -- overides paste buffer to last cut buffer
 
 -- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<leader>j", "<ESC>", opts)
